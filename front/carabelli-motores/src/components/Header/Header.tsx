569816cdede60aee2480ae26c24477671a8aa0b1
ignacio <ignacio.tumini@hotmail.com>
2024-08-15 15:57:12 -0300

    update header

--- front/carabelli-motores/src/components/Header/Header.tsx
+++ front/carabelli-motores/src/components/Header/Header.tsx
@@ -1,22 +1,37 @@
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import "./Header.css";
+import logo from "../../../public/WEB CARABELLI.png";
 
 function Header() {
   return (
     <Navbar expand="lg" className="navbar">
       <Container>
-        <Navbar.Brand href="#home">Carabelli Motores</Navbar.Brand>
+        <div>
+          <img src={logo} alt="Logo de carabelli motores" className="logo" />
+        </div>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Inicio</Nav.Link>
-            <Nav.Link href="#link">Nosostros</Nav.Link>
-            <NavDropdown title="Servicios" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Filtros</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
+            <Nav.Link href="#home" className="letras_nav">
+              Inicio
+            </Nav.Link>
+            <Nav.Link href="#link" className="letras_nav">
+              Nosostros
+            </Nav.Link>
+            <NavDropdown
+              title="Servicios"
+              id="basic-nav-dropdown"
+              className="letras_nav"
+            >
+              <NavDropdown.Item href="#action/3.1" className="letras_nav">
+                Filtros
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2" className="letras_nav">
                 Another action
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3" className="letras_nav">
+                Something
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
