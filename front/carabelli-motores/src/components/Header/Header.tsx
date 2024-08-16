0a655d9f2ea7bc20f9ab65cd61a21d33d184d731
ManuelForneri <manuelforneri@hotmail.com>
2024-08-16 12:41:37 -0300

    fixed navbar

--- front/carabelli-motores/src/components/Header/Header.tsx
+++ front/carabelli-motores/src/components/Header/Header.tsx
@@ -4,39 +4,49 @@ import logo from "../../../public/WEB CARABELLI.png";
 
 function Header() {
   return (
-    <Navbar expand="lg" className="navbar">
-      <Container>
-        <div>
+    <div>
+      <Navbar expand="lg" className="navbar ">
+        <Container className="m-0 w-100">
           <img src={logo} alt="Logo de carabelli motores" className="logo" />
-        </div>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home" className="letras_nav">
-              Inicio
-            </Nav.Link>
-            <Nav.Link href="#link" className="letras_nav">
-              Nosostros
-            </Nav.Link>
-            <NavDropdown
-              title="Servicios"
-              id="basic-nav-dropdown"
-              className="letras_nav"
-            >
-              <NavDropdown.Item href="#action/3.1" className="letras_nav">
-                Filtros
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2" className="letras_nav">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3" className="letras_nav">
-                Something
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home" className="text-white">
+                Inicio
+              </Nav.Link>
+              <Nav.Link href="#productos" className="text-white">
+                Productos
+              </Nav.Link>
+              <NavDropdown
+                title="Servicios"
+                id="basic-nav-dropdown"
+                className="letras_nav"
+              >
+                <NavDropdown.Item href="#action/3.1" className="text-white">
+                  Filtros
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2" className="text-white">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3" className="text-white">
+                  Something
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#link" className="text-white">
+                Galeria
+              </Nav.Link>
+              <Nav.Link href="#link" className="text-white">
+                Nosostros
+              </Nav.Link>
+              <Nav.Link href="#link" className="text-white">
+                Contacto
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div className="linea"></div>
+    </div>
   );
 }
 
